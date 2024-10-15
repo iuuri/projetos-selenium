b5c7ad91511ee949328fd85f15c9b928a50fb0e5
Iuri Souza <iuri.souzasantos@gmail.com>
2024-10-15 15:08:47 -0300

    Novos metodos para localizar elementos

--- localizar_elementos.py
+++ localizar_elementos.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.by import By
 
 def iniciar_driver():
     chrome_options = Options()
-    arguments = ['--lang=pt-BR', '--window-size=600,600', '--incognito']
+    arguments = ['--lang=pt-BR', '--window-size=1000,700', '--incognito']
 
     for argument in arguments:
         chrome_options.add_argument(argument)
@@ -43,11 +43,55 @@ driver = iniciar_driver()
 #Acessar site
 driver.get('https://cursoautomacao.netlify.app/')
 
-#Localizar elemento por id 
-btn_alerta = driver.find_element(By.ID, 'buttonalerta')
+# #Localizar elemento por id 
+# btn_alerta = driver.find_element(By.ID, 'buttonalerta')
 
-if btn_alerta is not None:
-    print('Botão encontrado.')
+# if btn_alerta is not None:
+#     print('Botão encontrado.')
+
+# #Localizar elemento por CLASS NAME
+# logo = driver.find_element(By.CLASS_NAME, 'navbar-brand')
+# nav = driver.find_element(By.CLASS_NAME, 'nav-link')
+
+# if logo is not None:
+#     print('Logo encontrado.')
+# if nav is not None:
+#     print('Navegação encontrada')
+
+# #Localizar elemento por NAME
+# campo_nome = driver.find_element(By.NAME, 'seu-nome')
+# radio_btn = driver.find_elements(By.NAME,'exampleRadios')
+
+# if campo_nome is not None:
+#     print('Botão seu nome encontrado.')
+# if radio_btn is not None:
+#     print('Encontramos os radios buttons')
+
+# #Localizar elemento por TEXTO em LINKS
+# link_home = driver.find_element(By.LINK_TEXT, 'Home')
+# link_parcial = driver.find_element(By.PARTIAL_LINK_TEXT,'Des')
+
+# if link_home is not None:
+#     print('Link home encontrado.')
+# if link_parcial is not None:
+#     print('Link parcial encontrado')
+
+# #Localizar elemento por TEXTO
+# texto = driver.find_element(By.XPATH, "//*[text()='ZONA DE TESTES']")
+
+# if texto is not None:
+#     print(texto.text)
+
+# # #Localizar elemento por TAG
+# tag_titulo = driver.find_element(By.TAG_NAME, 'h1')
+# tag_titulos = driver.find_elements(By.TAG_NAME, 'h4')
+
+# if tag_titulos is not None:
+#     print(tag_titulo.text) 
+
+
+
+
 
 input('aperta uma tecla para fechar')
 
