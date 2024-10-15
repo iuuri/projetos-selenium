fc3a70ab3743bfed9a3093168dc0c89635dca468
Iuri Souza <iuri.souzasantos@gmail.com>
2024-10-15 10:18:54 -0300

    Pimeiro commit - codigo base para automação selenium

---
+++ code_base.py
@@ -0,0 +1,46 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+def iniciar_driver():
+    chrome_options = Options()
+    arguments = ['--lang=pt-BR', '--window-size=600,600', '--incognito']
+
+    for argument in arguments:
+        chrome_options.add_argument(argument)
+
+    '''
+    --start-maximized # Inicia maximizado
+    --lang=pt-BR # Define o idioma de inicialização, # en-US , pt-BR
+    --incognito # Usar o modo anônimo
+    --window-size=800,800 # Define a resolução da janela em largura e altura
+    --headless # Roda em segundo plano(com a janela fechada)
+    --disable-notifications # Desabilita notificações
+    --disable-gpu # Desabilita renderização com GPU
+    '''
+
+    # Lista de opções experimentais(nem todas estão documentadas) https://chromium.googlesource.com/chromium/src/+/master/chrome/common/pref_names.cc
+    # Uso de configurações experimentais
+    chrome_options.add_experimental_option('prefs', {
+        # Alterar o local padrão de download de arquivos
+        'download.default_directory': 'C:\\Users\\iuri.santos\\Desktop\\Projetos\\Curso\\projetos-selenium',
+        # notificar o google chrome sobre essa alteração
+        'download.directory_upgrade': True,
+        # Desabilitar a confirmação de download
+        'download.prompt_for_download': False,
+        # Desabilitar notificações
+        'profile.default_content_setting_values.notifications': 2,
+        # Permitir multiplos downloads
+        'profile.default_content_setting_values.automatic_downloads': 1,
+    })
+
+
+    # inicializando o webdriver
+    driver = webdriver.Chrome(options=chrome_options)
+    return driver
+driver = iniciar_driver()
+
+
+driver.get('https://pt.wikipedia.org/wiki/Brasil')
+
+input('aperta uma tecla para fechar')
+
